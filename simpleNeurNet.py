1a22dfcd3dfe6372d515514286300334b9721e1a
ftarantelli <francesco.tarantelli@phd.unipi.it>
2023-07-18 20:37:26 +0200

    update  simpleNeurNet.py

--- simpleNeurNet.py
+++ simpleNeurNet.py
@@ -1,7 +1,72 @@
 import torch
-from torch import nn
-from matplotlib import pyplot as plt
+import torch.nn as nn
+import matplotlib.pyplot as plt
 
+#torch.manual_seed(6723)
+
+class netNeur(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(2, 16)
+        self.activation1 = nn.ReLU()
+        self.layer2 = nn.Linear(16, 8)
+        self.activation2 = nn.Tanh()
+        self.layer3 = nn.Linear(8, 1)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.activation1(x)
+        x = self.layer2(x)
+        x = self.activation2(x)
+        x = self.layer3(x)
+        return x
+
+def circle(x):
+    r = torch.norm(x, dim=1)
+    r = r.unsqueeze(1)
+    return torch.exp(-(r - 3) ** 2)
+
+loss_fun = nn.MSELoss()
+
+rete = netNeur()
+
+losses = []
+optimizer = torch.optim.SGD(rete.parameters(), lr=0.001)
+
+for i in range(0, 5000):
+    p = 3. + torch.randn((30, 2)) * 0.4
+    target = circle(p)
+    result = rete(p)
+    loss = loss_fun(target, result)
+    losses.append(loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    #rete.zero_grad()
+
+plt.plot(losses)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.show()
+
+p = 3. + torch.randn((1, 2)) * 0.2
+
+rete.eval()
+
+with torch.no_grad():
+    predictions = rete(p)
+
+print("Input p:")
+print(p)
+print("Predictions:")
+print(predictions)
+print("Circle Function Output:")
+print(circle(p))
+
+
+
+'''
 class netNeur(nn.Module):
 
 	def __init__(self):
@@ -24,8 +89,13 @@ class netNeur(nn.Module):
 
 
 def circle(x):
-	r = torch.norm(x)
+	#print(x)
+	r = torch.norm(x, dim = 1)
+	r = r.unsqueeze(1)
+	#print(r)
 	#theta = nn.arctan(x[0]/x[1])
+	#print(torch.exp(-(r-3)**2))
+	#sys.exit()
 	return torch.exp(-(r-3)**2)
 
 loss_fun=nn.MSELoss()
@@ -34,10 +104,11 @@ rete=netNeur()
 
 losses = []
 for i in range(0,5000):
-	p = torch.randn((100,2))*10
+	p = 3. + torch.randn((10,2))*0.2
 	target=circle(p)
 	result=rete.forward(p)
-
+	#print(target, result)
+	#sys.exit()
 	loss=loss_fun(target, result)
 	losses.append(loss.item())
 	loss.backward()
@@ -49,3 +120,16 @@ for i in range(0,5000):
 
 plt.plot(losses)
 plt.show()
+
+p = 3. + torch.randn((1,2))*0.2
+
+rete.eval()
+
+with torch.no_grad():
+    predictions = rete(p)
+
+# Print the predictions
+print(p)
+print(predictions)
+print(circle(p))
+'''
